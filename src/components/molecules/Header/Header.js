d946817f77944f0b606c86956d75366448f7ac4c
Alexander John <96aljohn@gmail.com>
2024-07-23 20:01:04 -0700

    update

--- src/components/molecules/Header/Header.js
+++ src/components/molecules/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <Text>Hello World</Text>
+            <Text>To the moon!</Text>
         </header>
     );
 };
